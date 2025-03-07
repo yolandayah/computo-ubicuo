8d302a6445f82971c002b426b0ef27ee57503862
Yolanda Alejo Huerta <za240113432@zapopan.tecmm.edu.mx>
2025-03-07 17:40:20 -0600

    iniciando tarea 5

---
+++ tarea05/edge_y_cloud.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+Simulación de dispositivos IoT que generan datos
+"""
+
+import random
+import time
+
+import pandas as pd
+
+
+def edge_device(device_id):
+    data = {
+        "timestamp": [],
+        "divice_id": [],
+        "temperature": [],
+        "humidity": [],
+    }
+    # Simulación de 10 lecturas de sensor
+    for _ in range(10):
+        timestamp = time.time()
+        temperature = random.uniform(20.0, 30.0)  # nosec B311 blacklist
+        humidity = random.uniform(30.0, 70.0)  # nosec B311 blacklist
+        data["timestamp"].append(timestamp)
+        data["device_id"].append(device_id)
+        data["temperature"].append(temperature)
+        data["humidity"].append(humidity)
+        # Espera de 1 segundo entre lecturas
+        time.sleep(1)
+    return pd.DataFrame(data)
+
+
+def main():
+    """
+    Comentario de la función
+    """
+    print("Hola Mundo")
+
+
+if __name__ == "__main__":
+    main()
